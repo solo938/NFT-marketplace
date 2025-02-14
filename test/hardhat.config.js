48298e1d636f9ae2e8c205156a26ddc062e5e486
solo938 <68711966+solo938@users.noreply.github.com>
2025-02-14 19:30:14 +0530

    Update hardhat.config.js

--- test/hardhat.config.js
+++ test/hardhat.config.js
@@ -1,1 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
 
+module.exports = {
+    solidity: "0.8.4",
+    networks: {
+        hardhat: {},
+        goerli: {
+            url: process.env.ALCHEMY_GOERLI_URL,
+            accounts: [process.env.PRIVATE_KEY],
+        }
+    }
+};
