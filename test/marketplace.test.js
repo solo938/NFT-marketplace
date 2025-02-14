96b770d1ede95ee4d13671a9ea7efde0563185ce
solo938 <68711966+solo938@users.noreply.github.com>
2025-02-14 19:34:49 +0530

    Update marketplace.test.js

--- test/marketplace.test.js
+++ test/marketplace.test.js
@@ -1,1 +1,23 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
+describe("Marketplace", function () {
+    it("Should create and execute market sales", async function () {
+        const [seller, buyer] = await ethers.getSigners();
+        const NFT = await ethers.getContractFactory("NFT");
+        const nft = await NFT.deploy();
+        await nft.deployed();
+
+        const Marketplace = await ethers.getContractFactory("Marketplace");
+        const marketplace = await Marketplace.deploy(2);
+        await marketplace.deployed();
+
+        await nft.mint("tokenURI");
+        await nft.approve(marketplace.address, 1);
+        await marketplace.makeItem(nft.address, 1, ethers.utils.parseEther("1"));
+
+        await marketplace.connect(buyer).purchaseItem(1, { value: ethers.utils.parseEther("1.02") });
+        const item = await marketplace.items(1);
+        expect(item.sold).to.equal(true);
+    });
+});
