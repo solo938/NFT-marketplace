3c29ec4b74db7ce70694632569a73cd1f52f6542
solo938 <68711966+solo938@users.noreply.github.com>
2025-02-14 19:28:32 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,20 @@
+const hre = require("hardhat");
 
+async function main() {
+    const NFT = await hre.ethers.getContractFactory("NFT");
+    const Marketplace = await hre.ethers.getContractFactory("Marketplace");
+
+    const nft = await NFT.deploy();
+    await nft.deployed();
+    console.log("NFT contract deployed to:", nft.address);
+
+    const feePercent = 2;
+    const marketplace = await Marketplace.deploy(feePercent);
+    await marketplace.deployed();
+    console.log("Marketplace contract deployed to:", marketplace.address);
+}
+
+main().catch(error => {
+    console.error(error);
+    process.exit(1);
+});
